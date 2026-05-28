6ca7ccc727a08d695337779fdb807fea68abce22
Davi Alcanfor Franco <alcanfordavi@gmail.com>
2026-05-28 00:24:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,8 @@ obsidian = ObsidianAPI()
 
 
 def on_send(raw_text: str, file_name: str) -> bool:
-    """
-    Sends a formatted note to Obsidian. 
-    This is the function that gets called when the user clicks the "Send to Obsidian"
-
-    Args:
-        raw_text (str): The raw text input from the user.
-        file_name (str): The name of the file to be created in Obsidian.
-
-    Returns:
-        bool: True if the note was successfully sent to Obsidian, False otherwise.
-    """
-
     formatted = agent.format_note(raw_text)
-
+    
     return obsidian.send_to_obsidian(file_name, formatted)
 
 
